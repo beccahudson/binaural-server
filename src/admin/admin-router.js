5f1a731a4f1ec07e8cb10b8a21d249602a9dca73
SeyiAriyo <58642769+SeyiAriyo@users.noreply.github.com>
2020-09-13 21:26:32 -0400

    Update admin-router.js

--- src/admin/admin-router.js
+++ src/admin/admin-router.js
@@ -6,7 +6,7 @@ const bodyParser = express.json();
 
 const adminRouter = express.Router();
 
-adminRouter.route("/api/:user")
+adminRouter.route("/api/:user/")
     .all(requireAuth)
     .get((req, res, next) => {
         AdminService.getAllUsers(req.app.get('db'))
@@ -20,11 +20,28 @@ adminRouter.route("/api/:user")
     .delete(bodyParser, (req, res, next) => {
         const { id, email } = req.body;
 
+        console.log(req.params)
+
         AdminService.deleteUser(req.app.get("db"), id, email)
             .then(() => {
                 res.status(204);
             })
             .catch(next);
     });
+
+    // adminRouter.route("/api/:id/:email")
+    // .all(requireAuth)
+    // .delete(bodyParser, (req, res, next) => {
+    //     const { id, email } = req.params;
+
+    //     console.log(req.params)
+
+    //     AdminService.deleteUser(req.app.get("db"), id, email)
+    //         .then(() => {
+    //             res.status(204);
+    //         })
+    //         .catch(next);
+    // });
+
 
 module.exports = adminRouter;
